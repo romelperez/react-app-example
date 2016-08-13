96cf75a1edfc8f8ed5629a15e8db97da41d2e34f
Romel Pérez <ronelprhone@gmail.com>
2016-08-13 11:55:53 -0500

    chore: webpack

---
+++ webpack.base.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    alias: {
+      tools:      path.resolve(__dirname + '/src/tools'),
+      i18n:       path.resolve(__dirname + '/src/i18n'),
+      components: path.resolve(__dirname + '/src/components'),
+      settings:   path.resolve(__dirname + '/src/settings.js'),
+    }
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      include: path.resolve(__dirname + '/src'),
+      exclude: /(\/lib\/|node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+const webpackBase = require('./webpack.base.js');
+const pkg = require('./package.json');
+const log = require('./api/log');
+
+const project = pkg.project;
+const dev = process.env.NODE_ENV !== 'production';
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(
+      dev ? 'development' : 'production'
+    )
+  })
+];
+
+log.env();
+
+if (!dev) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  );
+}
+
+module.exports = Object.assign({}, webpackBase, {
+  entry: {
+    core: [
+      'babel-polyfill',
+      './src/core/core.js'
+    ],
+    index: './src/index/index.js',
+  },
+  output: {
+    path: './dist/js/',
+    filename: '[name].js'
+  },
+  devtool: dev ? '#inline-source-map' : undefined,
+  plugins
+});
