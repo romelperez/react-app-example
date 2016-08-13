c687e919c41ba309b912426ba2ad4c7527b52059
Romel Pérez <ronelprhone@gmail.com>
2016-08-13 17:20:56 -0500

    test: refactor

---
+++ test/api/endpoint1/get-test.js
@@ -0,0 +1,10 @@
+describe('POST /endpoint1', function () {
+
+  it('params1', function () {
+    expect(50).to.be.equal(50);
+  });
+
+  it('params2', function () {
+    expect(true).to.be.true;
+  });
+});
