9311aff9a31d80513040e49aa0aad9545985715d
Romel Pérez <ronelprhone@gmail.com>
2016-08-22 19:44:52 -0500

    refactor: server router

--- server/router.js
+++ server/router.js
@@ -2,22 +2,50 @@ import fs from 'fs';
 import path from 'path';
 import log from './log';
 
-const src = path.join(__dirname, '/api/urls');
+const srcs = [
+  path.join(process.cwd(), '/server/urls'),
+  path.join(process.cwd(), '/server/api')
+];
 
-export default function (app) {
+const load = function (app, src) {
 
-  fs.readdirSync(src).
-    filter(file => fs.statSync(path.join(src, file)).isDirectory()).
-    forEach(folder => {
-      const definition = require(src +'/'+ folder).default;
+  const getItems = dirPath => {
+    return fs.readdirSync(dirPath).map(item => {
+      return path.join(dirPath, item);
+    });
+  };
+  const isFolder = itemPath => fs.statSync(itemPath).isDirectory();
+  const isFile = itemPath => !isFolder(itemPath);
 
-      Object.keys(definition).forEach(function (location) {
-        const route = app.route(location);
-        const verbs = definition[location];
+  const defineRouters = definition => {
+    if (!definition) return;
+    Object.keys(definition).forEach(function (url) {
+      const route = app.route(url);
+      const verbs = definition[url];
 
-        Object.keys(verbs).forEach(function (verb) {
-          route[verb].call(route, verbs[verb]);
-        });
+      Object.keys(verbs).forEach(function (verb) {
+        route[verb].call(route, verbs[verb]);
       });
     });
+  };
+
+  const processFile = filePath => {
+    const definition = require(filePath);
+    defineRouters(definition ? definition.default : definition);
+  };
+
+  const items = getItems(src);
+
+  items.filter(isFile).forEach(processFile);
+
+  items.filter(isFolder).forEach(folder => {
+    const subItems = getItems(folder);
+    subItems.filter(isFile).forEach(processFile);
+  });
 };
+
+const init = function (app) {
+  srcs.forEach(src => load(app, src));
+};
+
+export default init;
