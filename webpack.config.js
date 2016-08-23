87ef3674317c2f44ebe355b8ab002e1f9523cf5f
Romel Pérez <ronelprhone@gmail.com>
2016-08-22 20:32:15 -0500

    chore: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -26,9 +26,13 @@ if (!dev) {
 
 module.exports = Object.assign({}, webpackBase, {
   entry: {
-    core: './src/js/core/index.js',
+    'core': './src/js/core/index.js',
     'static-index': './src/js/static/index/index.js',
     'static-terms-and-conditions': './src/js/static/terms-and-conditions/index.js',
+    'users-cars': './src/js/users/cars/index.js',
+    'users-login': './src/js/users/login/index.js',
+    'users-register': './src/js/users/register/index.js',
+    'cms': './src/js/cms/index.js',
   },
   output: {
     path: './dist/js/',
