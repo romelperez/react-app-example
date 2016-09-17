2294a590f1a71449698968bf94ab4440aef99df8
Romel Pérez <ronelprhone@gmail.com>
2016-09-16 21:52:07 -0500

    update: src login

--- src/users/login/containers/LoginForm/index.js
+++ src/users/login/containers/LoginForm/index.js
@@ -1,10 +1,20 @@
 import React, { PropTypes } from 'react';
-import i18n from 'i18n';
-import data from 'data';
-import Card from 'components/general/Card';
-import Callout from 'components/general/Callout';
+
+import i18n         from 'i18n';
+import data         from 'data';
+import constants    from 'constants';
+import settings     from 'settings';
+import Card         from 'components/general/Card';
+import Callout      from 'components/general/Callout';
 
 const LoginForm = React.createClass({
+
+  getInitialState () {
+    return {
+      error: false,
+      message: ''
+    };
+  },
 
   componentDidMount () {
     $(this.form).vulcanval({
@@ -28,6 +38,22 @@ const LoginForm = React.createClass({
     const password =  i18n.t('login.password');
     const enter =     i18n.t('login.enter');
 
+    let errorEl;
+    if (this.state.error) {
+      const onClose = e => {
+        this.setState({ error: false });
+      };
+      errorEl = (
+        <div className='row'>
+          <div className='column small-12'>
+            <Callout onClose={onClose}>
+              <b>Error:</b> {this.state.message}
+            </Callout>
+          </div>
+        </div>
+      );
+    }
+
     return (
       <Card className='login-form'>
         <form ref={ref => this.form = ref} onSubmit={this.onSubmit}>
@@ -36,18 +62,21 @@ const LoginForm = React.createClass({
               <h2>{title}</h2>
             </div>
           </div>
+          {errorEl}
           <div className='row'>
             <div className='column small-12'>
               <label htmlFor='login-form__email'>{email}</label>
               <input id='login-form__email' name='email' type='email'
-                     placeholder={email} />
+                placeholder={email} data-vv-display='#login-form__email-display' />
+              <div id='login-form__email-display' />
             </div>
           </div>
           <div className='row'>
             <div className='column small-12'>
               <label htmlFor='login-form__password'>{password}</label>
               <input id='login-form__password' name='password' type='password'
-                     placeholder={password} />
+                placeholder={password} data-vv-display='#login-form__password-display' />
+              <div id='login-form__password-display' />
             </div>
           </div>
           <div className='row text-center'>
@@ -66,26 +95,34 @@ const LoginForm = React.createClass({
 
     const invalids = $(this.form).vulcanval('inspect');
     if (!invalids) {
-      this.login();
+      this.login(e);
     }
 
     return false;
   },
 
-  login () {
+  onError (e, nextState) {
+    this.setState(nextState);
+  },
+
+  onSuccess (e) {
+    window.location.href = constants.ROUTE.CARS;
+  },
+
+  login (e) {
 
     const map = $(this.form).vulcanval('getMap');
 
-    // TODO:
     data.
       login(map).
       then(response => {
-        // DEBUG:
-        console.log('login success', response);
+        this.onSuccess(e);
       }).
-      catch(response => {
-        // DEBUG:
-        console.log('login error', response);
+      catch(error => {
+        this.onError(e, {
+          error: true,
+          message: error.response.data.message,
+        });
       });
   },
 });
